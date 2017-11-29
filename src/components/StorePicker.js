dedbdbfae63fc98652d3b2b80d65bc861652a0c5
xav <xavierartot@gmail.com>
2017-11-29 17:10:08 -0600

    second video

---
+++ src/components/StorePicker.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import XavTest from './XavTest';
+class StorePicker extends React.Component {
+  render() {
+    let test = 5555;
+    //sdfsdff
+    return (
+      <div className='wrapper'>
+        {/* jsx */}
+        {/* hello */}
+        <form action='' class='store-selector' method='post'>
+          Hello X
+          <p> ddd {test}</p>
+          <div>
+            <XavTest/> 
+          </div>
+        </form>
+      </div>
+      )
+  }
+}
+export default StorePicker;
---
+++ src/components/XavTest.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class XavTest extends React.Component {
+  render() {
+    return (
+      <p>c'est la vie de Xavier</p>
+      )
+  }
+}
+
+export default XavTest;
